5df056ebb83f38f0e1339f313052bd73404e073e
hema <shaswarhema@gmail.com>
2024-09-09 17:57:50 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use rust_io::*;
+use std::{env, process};
+
+fn main() {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    run(config);
+}
